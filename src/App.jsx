62ef00b74de40b7772d095792b0d61aa25715ac5
Amrit-Raj-Biswal <amritrajbiswal2019@gmail.com>
2025-10-15 23:21:24 +0530

    Partially Responsive Frontend Spotify Clone

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
+const App = () => {
+
+  const {audioRef, track} = useContext(PlayerContext)
+
+  return (
+    // w-20 -- 5 rem -- 80 px
+    // h-10 -- 2.5 rem -- 40 px
+    // bg-neutral-500 -- {r, g, b} = {115, 115, 115}
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar/>
+        <Display/>
+      </div> 
+        <Player/>
+        <audio ref={audioRef} preload='auto' src={track.file}></audio>
+       </div>
+  )
+}
+
+export default App
